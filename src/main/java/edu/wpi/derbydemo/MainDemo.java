c4d9ad06c7ac3973f15b4c13a2000e7197781ae3
jiananli306 <33046262+jiananli306@users.noreply.github.com>
2020-01-30 02:01:00 -0500

    JohnDemo2

--- src/main/java/edu/wpi/derbydemo/MainDemo.java
+++ src/main/java/edu/wpi/derbydemo/MainDemo.java
@@ -3,6 +3,7 @@ package edu.wpi.derbydemo;
 import utils.JDBCutils;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -17,31 +18,38 @@ public class MainDemo {
 
         Connection conn = null;
         Statement stmt = null;
+        PreparedStatement pstmt = null;
+
+
         try {
             conn = JDBCutils.getConnection();
             stmt = conn.createStatement();
+
             JDBCutils.dropTable("Nodes", stmt);
             JDBCutils.createNodeTable(stmt);
+
+            ArrayList<String[]> al = db.readCsvFile(db.getNodecsvPath());
+
+            String sql = "INSERT INTO Nodes  VALUES(?,?,?,?,?,?,?,?,?)";
+
+            for (String[] array : al) {// This loop is used to iterate through the arraylist
+                  pstmt = conn.prepareStatement(sql);
+                for (int i=0; i<array.length;i++) {//This loop is used to iterate through the array inside the arraylist
+                    if(array[i].chars().allMatch(Character::isDigit)){
+                        pstmt.setInt(i+1,Integer.parseInt(array[i]));
+                    }else{
+                        pstmt.setString(i+1,array[i]);
+                    }
+                }
+                pstmt.executeUpdate();
+            }
+
 
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
             JDBCutils.close(conn, stmt);
         }
-        System.out.println("John");
-
-
-        ArrayList<String[]> al = db.readCsvFile(db.getNodecsvPath());
-
-
-
-
-
-
-
-
-
-
 
     }
 
