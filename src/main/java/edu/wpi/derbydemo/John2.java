9882b0d68b6700b7c8e902ab09db2d5a3c158739
xchen0326 <xchen10@wpi.edu>
2020-02-01 00:34:30 -0500

    Cleaned up

--- src/main/java/edu/wpi/derbydemo/John2.java
+++
@@ -1,8 +0,0 @@
-package edu.wpi.derbydemo;
-
-public class John2 {
-
-    public static void main(String[] args){
-        System.out.println("adsfasdfasfa");
-    }
-}
--- src/main/java/edu/wpi/utils/JDBCutils2.java
+++
@@ -1,4 +0,0 @@
-package edu.wpi.utils;
-
-public class JDBCutils2 {
-}
